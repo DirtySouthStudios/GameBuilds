639fb9137f82e621fec0ea9e2f70ad4fdda47bfe
FrankyG170 <fran922003@yahoo.com>
2014-06-26 12:05:17 -0400

    Patrol

--- Patrol.cs
+++ Patrol.cs
@@ -1,17 +1,24 @@
-// Francesco
-// 6/19/2014
-// 6/24/2014
-// Prototype2\Assets\Scripts
-// Enemy Prefab object
+// Credit: SharpDevelopment for overshoot method
+// Francesco Maoli
+// Attach to Enemy Prefab
+// Place in Assets/Scripts
+// Created 6/19/2014
+// Last Update 6/26/2014
 
 using UnityEngine;
 using System.Collections;
 
-public class Patrol : MonoBehaviour {
-
+public class Patrol : GameManager {
+	
 	public float moveSpeed;
 
-	// Point Array Navigation variables
+	// Last Position Navigation variables 
+	private Transform _playerTransform, _transform;
+	private Vector3 _targetPosition, _forward;
+	private bool _isWaiting;
+	public float overshoot, waitTime;
+
+	// Point Array Navigation
 	public Transform[] patrolPoints;
 	private int currentPoint;
 
@@ -22,47 +29,79 @@ public class Patrol : MonoBehaviour {
 
 	void Start () {
 
-		//Locate Patrol Points
-		transform.position = patrolPoints [0].position;
-		currentPoint = 0;
+		if (currentLevel == 0) {
+			_playerTransform = GameObject.FindGameObjectWithTag ("Player").transform;
+			_transform = transform;
 
-		// Locate Player
-		GameObject player = GameObject.FindGameObjectWithTag("Player");
-		target = player.transform;
+			UpdateMovement ();
+		}
 
-		// Locate Player Position
-		targetPosition = player.transform.position;
+		if (currentLevel == 1) {
+			//Locate Patrol Points
+			transform.position = patrolPoints [0].position;
+			currentPoint = 0;
+		}
 
-		// Enemy Current Position
-		GameObject enemy = this.gameObject;
-		enemyTransform = enemy.transform;
+		if (currentLevel == 2) {
+			// Locate Player
+			GameObject player = GameObject.FindGameObjectWithTag("Player");
+			target = player.transform;
+
+			// Locate Player Position
+			targetPosition = player.transform.position;
+
+			// Enemy Current Position
+			GameObject enemy = this.gameObject;
+			enemyTransform = enemy.transform;
+		}
 	}
 	
 
 	void Update () {
 
-		// Follow Player Indefinitely [Works]
-		/* transform.LookAt(target);
-		enemyTransform.position += enemyTransform.forward * moveSpeed * Time.deltaTime; */
+		if (currentLevel == 0) {
+			if (_isWaiting)
+					return;
 
+			_transform.position += _forward * moveSpeed * Time.deltaTime;
 
-		// Array Point Navigation [Works]
-		/*if(transform.position == patrolPoints[currentPoint].position){
-			currentPoint ++;
-		}
-		
-		if (currentPoint >= patrolPoints.Length){ //If currentPoint is greater than the total amount of points
-			currentPoint = 0;	//Set currentPoint back to zero
+			if (Vector3.Dot (_forward, _targetPosition - _transform.position) < 0) {
+					StartCoroutine (Wait ());
+			}
 		}
 
-		transform.position = Vector3.MoveTowards(transform.position, patrolPoints [currentPoint].position, moveSpeed * Time.deltaTime);
-		*/
+		if (currentLevel == 1) {
+			// Follow Player Indefinitely [Works]
+			transform.LookAt (target);
+			enemyTransform.position += enemyTransform.forward * moveSpeed * Time.deltaTime;
+		}
+
+		if (currentLevel == 2) {
+			// Array Point Navigation [Works]
+			if (transform.position == patrolPoints [currentPoint].position) {
+					currentPoint ++;
+			}
+
+			if (currentPoint >= patrolPoints.Length) {
+					currentPoint = 0;	
+			}
+
+			transform.position = Vector3.MoveTowards (transform.position, patrolPoints [currentPoint].position, moveSpeed * Time.deltaTime);
+		}
 	}
 
-	// Go to Player Position, then find new position of player [WIP], has yet to our intentions
-	/*void GoToPlayerPos(){
-			transform.LookAt (targetPosition);
-			enemyTransform.position += enemyTransform.forward * moveSpeed * Time.deltaTime;
-			enemyTransform.position += enemyTransform.forward * 1.2f * Time.deltaTime;
-	}*/
+	// Updates player position and calculates the overshot position
+	void UpdateMovement() {
+		_transform.LookAt( _playerTransform );
+		_forward = _transform.forward; 
+		_targetPosition = _playerTransform.position + _forward*overshoot; 
+	}
+
+	// Used in conjuction with the UpdateMovement() method to create a "dumb" AI with waiting time
+	private IEnumerator Wait () {
+		_isWaiting = true;
+		yield return new WaitForSeconds(waitTime);
+		UpdateMovement();
+		_isWaiting = false;
+	}
 }

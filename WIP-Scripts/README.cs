19aac38c038ad71ec4c79010e09f5a2977e485a6
FrankyG170 <fran922003@yahoo.com>
2014-06-27 17:00:02 -0400

    Create README.cs

---
+++ WIP-Scripts/README.cs
@@ -0,0 +1,21 @@
+WIP-Scripts
+===========
+
+// Scripts that are Tested but not fully functional or require more coding for complete feature implementation.
+
+// Follow README in the GameBuilds section plus the follow guidelines:
+
+// If script is tested and has compiler errors
+
+  /* 1) Describe nature of the error
+     2) Copy/Paste error code from console to top of script in comment line
+     3) Comment you intentions with the non-functional code next to the line that is not working
+  */
+  
+
+// If script is tested and has no errors but still requires more coding for full implementation
+
+  /* 1) State at top goal of the script
+     2) Write in comments where the new code to finish script should be
+     3) Write what the required code must do in the comment as well
+  */

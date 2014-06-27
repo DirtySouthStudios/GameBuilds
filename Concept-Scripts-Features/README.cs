35f6597c1451611e6fa37e9393f46cd0bd72bda2
FrankyG170 <fran922003@yahoo.com>
2014-06-27 17:04:05 -0400

    Scripts that are not yet hard coded or tested

---
+++ Concept-Scripts-Features/README.cs
@@ -0,0 +1,20 @@
+Concept-Scripts-Features
+========================
+
+Scripts that are either Un-Tested or simple concepts that have yet to be hard coded.
+
+// Please use the same README.cs in GameBuilds along with these additional rules
+
+// New Concepts that have no hard coding should be made as follows:
+  
+    /* 1) Description of concept
+       2) Thought process behind code
+       3) Suggestions for variable/method names
+    */
+    
+// New Concepts that are hard coded should be made with the following:
+
+  /* 1) What needs to be done before it can be tested
+     2) What specifically is working and not working
+     3) Goal of the feature/script
+  */

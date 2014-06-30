375705e965cc492527ff5a3ae52bf9547afdb41d
FrankyG170 <fran922003@yahoo.com>
2014-06-30 17:01:32 -0400

    Update ObjectPooler.cs

--- WIP-Scripts/ObjectPooler.cs
+++ WIP-Scripts/ObjectPooler.cs
@@ -9,36 +9,36 @@ using System.Collections.Generic;
 public class ObjectPooler : MonoBehaviour {
 
 	public static ObjectPooler current;
-	public GameObject pooledObject;
+	public GameObject pooledObject; // This shows up in the inspector and is where the prefab is placed
 	public int pooledAmount = 10;
 	public bool willGrow = true;
 
 	List<GameObject> pooledObjects;
 
 	void Awake (){
-		current = this;
+		current = this; // Need this for other scripts that are dependent on the current state
 	}
 
 	void Start () {
-		pooledObjects = new List<GameObject> ();
+		pooledObjects = new List<GameObject> (); // We used list because it allows for dynamic features that array cannot do
 		for (int i = 0; i < pooledAmount; i++) {
-			GameObject obj = (GameObject)Instantiate (pooledObject);
-			obj.SetActive (false);
-			pooledObjects.Add (obj);
+			GameObject obj = (GameObject)Instantiate (pooledObject); //Create a pooledObject
+			obj.SetActive (false); // Make object inactive
+			pooledObjects.Add (obj); // Add it to the pool
 		}
 	}
-	
+	// Grabs a pooledObject when it is needed
 	public GameObject GetPooledObject () {
 		for(int i = 0; i < pooledObjects.Count; i++){
-			if(!pooledObjects[i].activeInHierarchy){
-				return pooledObjects[i];
+			if(!pooledObjects[i].activeInHierarchy){ // If the object is not active in the hierarchy
+				return pooledObjects[i]; // Make it active
 			}
 		}
-
+		// If 10 objects is not enough, grow the list
 		if(willGrow){
 			GameObject obj = (GameObject)Instantiate(pooledObject);
 			pooledObjects.Add(obj);
-			return obj;
+			return obj; // Return the object that was added
 		}
 		return null;
 	}

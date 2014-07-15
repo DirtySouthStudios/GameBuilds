06663481ae736e86374543ccff0874b57090369f
FrankyG170 <fran922003@yahoo.com>
2014-07-15 18:17:22 -0400

    Update JumpTest.cs

--- WIP-Scripts/JumpTest.cs
+++ WIP-Scripts/JumpTest.cs
@@ -1,6 +1,6 @@
 // Francesco Maoli & Yang Song
 // 7/7/2014
-// 7/7/2014
+// 7/15/2014
 // Assets/Scripts
 // Player Prefab
 
@@ -9,38 +9,46 @@ using System.Collections;
 
 public class JumpTest : MonoBehaviour {
 	
-	
+	// Jump variables
 	private Vector3 input;
 	public float jumpSpeed = 8f;
 	private float maxJumpSpeed = 10f;
 	public float gravity = 3f;
-
-	private float playerHeight;
-
+	
+	// Directional variable
 	private Vector3 moveDirection = Vector3.zero;
 
-
+	// Boolean test
+	private boolean grounded; // NEW
+	
 	void FixedUpdate () {
-
-
-		moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-		moveDirection = transform.TransformDirection(moveDirection);
-
-
-		// Needs Work
-		if (Input.GetButton ("space")) {
-			Debug.Log ("I jumped!");
-			moveDirection.y = jumpSpeed;
-			if (rigidbody.velocity.magnitude < maxJumpSpeed){
-				rigidbody.AddRelativeForce (moveDirection * jumpSpeed);
+		
+		
+		GroundedTest(); // NEW
+		
+		if (grounded){  // NEW
+		
+			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+			moveDirection = transform.TransformDirection(moveDirection);
+	
+			
+			// Needs Work
+			if (Input.GetButton ("space")) {
+				Debug.Log ("I jumped!");
+				moveDirection.y = jumpSpeed;
+				if (rigidbody.velocity.magnitude < maxJumpSpeed){
+					rigidbody.AddRelativeForce (moveDirection * jumpSpeed);
+				}
 			}
 		}
-
-		// Apply gravity
-		//moveDirection.y -= gravity * Time.deltaTime;
-
-		/*if (rigidbody.velocity.magnitude < maxHeight){
-		rigidbody.AddRelativeForce (input * jumpHeight);
-		}	*/
+		
+	}
+	
+	public GroundedTest(){ // NEW
+		OnCollisionEnter(Collision collision){
+			if (collision.gameObject.tag("Player")){
+				grounded = true;
+			}
+		}
 	}
 }
